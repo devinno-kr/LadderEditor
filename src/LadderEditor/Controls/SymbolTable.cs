3a01c667d39a23934b9793b5deec0ed1f0312dc5
devinno <devinno@devinno.kr>
2023-07-18 19:12:09 +0900

    -

--- src/LadderEditor/Controls/SymbolTable.cs
+++ src/LadderEditor/Controls/SymbolTable.cs
@@ -122,13 +122,14 @@ namespace LadderEditor.Controls
                             e.Graphics.DrawLine(Pens.Black, rtSC.Left, rtCol1.Top, rtSC.Left, rtCol1.Bottom);
                             e.Graphics.DrawLine(Pens.Black, rtScroll.Left, rtScroll.Top, rtScroll.Left, rtScroll.Bottom);
 
-                            if (Program.DataMgr.Language == Managers.Lang.KO)
+                            var lang = Program.DataMgr?.Language ?? Managers.Lang.KO;
+                            if (lang == Managers.Lang.KO)
                             {
                                 Theme.DrawText(e.Graphics, $"{m[i]} 영역", Font, Color.FromArgb(200, 200, 200), rtTitle);
                                 Theme.DrawText(e.Graphics, "주소", Font, Color.FromArgb(200, 200, 200), rtCol1);
                                 Theme.DrawText(e.Graphics, "명칭", Font, Color.FromArgb(200, 200, 200), rtCol2);
                             }
-                            else if (Program.DataMgr.Language == Managers.Lang.EN)
+                            else if (lang == Managers.Lang.EN)
                             {
                                 Theme.DrawText(e.Graphics, $"{m[i]} Area", Font, Color.FromArgb(200, 200, 200), rtTitle);
                                 Theme.DrawText(e.Graphics, "Address", Font, Color.FromArgb(200, 200, 200), rtCol1);
